c355924d85fb2740c7ecfcf9811e455014727477
Damian Kapczynski <damkapp2@gmail.com>
2025-04-13 17:20:51 +0200

    Adding tresholds to groups

--- groups.js
+++ groups.js
@@ -3,7 +3,9 @@ import { sleep, group, check } from 'k6';
 
 export const options = {
     thresholds: {
-        http_req_duration: ['p(95)<250']
+        http_req_duration: ['p(95)<250'],
+        'group_duration{group:::Main page}': ['p(95)<200'],
+        'group_duration{group:::Main page::Assets}': ['p(95)<200'],
     }
 }
 
